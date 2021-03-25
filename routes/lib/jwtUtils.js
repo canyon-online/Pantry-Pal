f8b75114b4f02e2a5f7a7ad5234d5b099118d391
jordanmcm <jmcmillan@knights.ucf.edu>
2021-03-25 15:54:23 -0400

    Add information to JWT payload and a verification function

--- routes/lib/jwtUtils.js
+++ routes/lib/jwtUtils.js
@@ -5,10 +5,12 @@ const jwt = require('jsonwebtoken');
 const jwtExpiryTime = process.env.JWT_EXPIRY_TIME * 1000;
 const jwtKey = process.env.JWT_SECRET_KEY;
 
+// TODO: convert to asynchronous implementations
+
 // Generate a JWT using the key provided and a user's object id
 // Currently a synchronous implementation
-function generateJWT(oid) {
-    const token = jwt.sign({ oid }, jwtKey, {
+function generateJWT(oid, name) {
+    const token = jwt.sign({ userId: oid, name: name }, jwtKey, {
         algorithm: 'HS256',
         expiresIn: jwtExpiryTime
     });
@@ -16,19 +18,34 @@ function generateJWT(oid) {
     return token;
 }
 
+// Verify a JWT, returning either the payload of the token or null
+// Currently a synchronous implementation
+function verifyJWT(token) {
+    try {
+        return jwt.verify(jwtKey, token);
+    } catch(err) {
+        return null;
+    }
+}
+
+
 // Function to generate and send a JWT 
 async function genAndSendToken(user, res) {
     // Generate a JWT using the user objectid
-    const token = await generateJWT(user._id);
+    const token = await generateJWT(user._id, user.display);
 
     // Send the user a JWT token to store to save their session
-    res.cookie("token", token, { maxAge: jwtExpiryTime });
+    res.cookie("token", token, { httpOnly: true, maxAge: jwtExpiryTime });
     res.json({ token: token, expiresIn: jwtExpiryTime });
 }
+
+
 
 // Export pertinent functions and objects related to JSON Web Tokens
 module.exports = {
     generateJWT: generateJWT,
     sendJWT: genAndSendToken,
+    verifyJWT: verifyJWT,
+
     maxAge: jwtExpiryTime
 };
