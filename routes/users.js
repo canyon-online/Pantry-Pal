2eb528d46a4879e1a8dd76a64cbf9b512de64e03
jordanmcm <jmcmillan@knights.ucf.edu>
2021-03-28 21:35:13 -0400

    Create blank files for remaining planned endpoints and fix some comment errors

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+function use(router) {
+    // All of these endpoints are authenticated actions
+    
+}
+
+// Export the use function, enabling the users endpoint
+module.exports = {
+    use: use
+};
