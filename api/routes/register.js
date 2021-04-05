47b04340a600b8efb0f75f6ebca8dfa57ca7e186
jordanmcm <jmcmillan@knights.ucf.edu>
2021-04-04 21:17:11 -0400

    Move local registration validation checks to the User schema

--- api/routes/register.js
+++ api/routes/register.js
@@ -11,62 +11,17 @@ const User = require('../models/user');
 const constructPath = require('./lib/constructpath');
 const endpointPath = '/register';
 
-// Validation function for local registration request bodies
-// TODO: condense into regex checks (can add regex to the user schema)
-// TODO: sanitize inputs
-function verifyLocal(body, res)
-{
-    // Content existence checks
-    // No body sent
-    if (!body) {
-        res.status(422).json({ error: "No body included in request" });
-        return false;
-    }
-
-    // No name sent
-    if (!body.name) {
-        res.status(422).json({ error: "No name included in request" });
-        return false;
-    }
-
-    // No password sent
-    if (!body.password) {
-        res.status(422).json({ error: "No password included in request" });
-        return false;
-    }
-
-    // No email sent
-    if (!body.email) {
-        res.status(422).json({ error: "No email included in request" });
-        return false;
-    }
-
-    // Content validation checks
-    // Name checks
-    if (body.name.length < 8 || body.name.length > 32) {
-        res.json({ error: "Display name must be between 8 and 32 characters" });
-        return false;
-    }
-
-    if (typeof(body.name) != "string") {
-        res.status(422).json({ error: "Display name must be a string" });
-        return false;
-    }
-
-    // Many of these could be solved with regex, so I will stop this here for now
-
-    return true;
-}
-
 // Assumed a user might not be logged in to access any of these endpoints
 // Registration is always an unauthenticated action initially
 function safeActions(router) {
     router.post(constructPath(endpointPath, '/'), async function(req, res) {
         // Verify neccessary information is provided
-        let isValid = await verifyLocal(req.body, res);
-        if (!isValid)
+        let isValid = (req.body && req.body.password)
+        if (!isValid) {
+            res.status(422).json({ error: "Missing required fields" });
             return;
-
+        }
+            
         // Begin registration process
         await bcryptUtil.encryptPassword(req.body.password, function(err, hash) {
             // If an error occured during password encryption, display an error
@@ -92,8 +47,8 @@ function safeActions(router) {
                 // Sends the error as output
                 if (err.keyPattern && err.keyPattern.email)
                     res.status(422).json({ error: "An account with this email already exists" });
-                //else if (err._message == "User validation failed")
-                //    res.status(422).json({ error: "Failed to create an account with this name or email" });
+                else if (err._message == "User validation failed")
+                    res.status(422).json({ error: "Failed to create an account with this name or email" });
                 else
                     res.status(422).json({ error: err });
             });
