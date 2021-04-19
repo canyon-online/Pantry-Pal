123a70be89e57f9e0aaf955cd92517fdb2ca0a38
jordanmcm <jmcmillan@knights.ucf.edu>
2021-04-19 19:26:52 -0400

    Clean Login and Account testing specs
    The multiple expectations might best be put into a spec helper or there might already be a useful spec helper for this, but for now I'm just cleaning what is here

--- api/spec/unit_testing/AccountSpec.js
+++ api/spec/unit_testing/AccountSpec.js
@@ -1,22 +1,44 @@
 describe("Account", function() {
-    var Account = require('../../routes/account');
+    const Account = require('../../routes/account');
+    const Code = require("../../models/verificationCode");
 
     // Constant testing values we will use for testing verification codes
     const CodeVals = {
-        code: "1234",
-        user: "userId",
+        code: "123456",
+        user: "605927d16847e332654180c7",
         purpose: "purpose"
     }
 
-    // Create a token
-    var token;
+    it("should return nothing if the specified code does not exist", function() {
+        // Returns a promise, so we have to fulfill the promise
+        var codePromise = Account.getCode(CodeVals.code, CodeVals.user, CodeVals.purpose);
+
+        // After fulfilling the promise, check
+        codePromise.then(function(code) {
+            expect(code).toEqual(undefined);
+        });
+    });
 
     it("should find and delete a verification code", function() {
+        // To ensure we find a valid verification code, we have to generate one
+        new Code({
+            code: CodeVals.code,
+            user: CodeVals.user,
+            purpose: CodeVals.purpose
+        }).save();
+
         // Attempt to find a verification code using parameters and delete it
         // Using example strings such as "userId" rather than actual ObjectIds, as it should not matter
-        token = Account.getCode(CodeVals.code, CodeVals.user, CodeVals.purpose);
+        var codePromise = Account.getCode(CodeVals.code, CodeVals.user, CodeVals.purpose);
 
-        // We expect there to be a code found
-        expect(token).toHaveCode(CodeVals.code);
+        codePromise.then(function(code) {
+            // Expect a code to have been found
+            expect(code).not.toEqual(undefined);
+
+            // Expect it to be the correct code
+            expect(code.code).toEqual(CodeVals.code);
+            expect(code.user).toEqual(CodeVals.user);
+            expect(code.purpose).toEqual(CodeVals.purpose);
+        });
     });
 });
--- api/spec/unit_testing/LoginSpec.js
+++ api/spec/unit_testing/LoginSpec.js
@@ -1,23 +1,64 @@
 describe("Login", function() {
-    var Login = require('../../routes/login');
+    const Login = require('../../routes/login');
+    const Response = require('../test_models/res');
 
-    // Test res object
-    const res;
+    // Test login user and response object
+    var res;
+    var loginVals;
 
-    // Constant
-    const LoginVals = {
-        body: "body",
-        res: res
-    }
+    // Create a new Express response object for every login test
+    beforeEach(function() {
+        res = new Response();
+        loginVals = {};
+    });
 
-    // Create a token
-    var token;
+    // Testing the verifyLocal function
+    describe("verification", function() {
+        it("should allow properly formatted data to be passed to bcrypt", function() {
+            // Properly formatted data
+            loginVals.body = { email: "email@email.com", password: "password" };
 
-    it("should ensure that passed data is formatted properly", function() {
-        // Ensure that the data is passed properly
-        token = Login.verifyLocal(LoginVals.body, LoginVals.res);
+            // Ensure that the data is passed properly
+            const testCall = Login.verifyLocal(loginVals.body, res);
+    
+            // We expect the return value of verifyLocal to be true if there was no error
+            // We also expect the response object to have certain properties after success
+            expect(testCall).toEqual(true);
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toEqual(undefined)
+        });
 
-        // We expect the return value of verifyLocal to be true if there was no error
-        expect(token).toBeVerified(LoginVals.res);
+        it("should disallow improperly formatted data to be passed to bcrypt", function() {
+            // Improperly formatted data
+            loginVals.body = { email: 12345, password: "password" };
+
+            // Ensure that the data is passed properly
+            const testCall = Login.verifyLocal(loginVals.body, res);
+
+            // We expect the return value of verifyLocal to be true if there was no error
+            // We also expect the response object to have certain properties after success
+            expect(testCall).toEqual(false);
+            expect(res.statusCode).not.toEqual(200);
+            expect(res.body).not.toEqual(undefined)
+        });
+
+        it("should disallow missing data from being passed to bcrypt", function() {
+            // Improperly formatted data
+            loginVals.body = undefined;
+
+            const testCall = Login.verifyLocal(loginVals.body, res);
+
+            // We expect the return value of verifyLocal to be false if there was an error
+            // We also expect the response object to have certain properties after an error
+            expect(testCall).toEqual(false);
+            expect(res.statusCode).not.toEqual(200);
+            expect(res.body).not.toEqual(undefined)
+        });
+    });
+
+    // Testing the loginLocal function
+    // Slightly less of a "unit test"
+    describe("locally", function() {
+
     });
 });
